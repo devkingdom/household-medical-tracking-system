5305e1cb647d60e9b8e5c65635eff9c457c683a0
paulin <spextreme@gmail.com>
2020-04-03 20:58:02 -0400

    Added source content to start from.

---
+++ src/main/java/com/devkingdom/householdmedicaltrackingsystem/HouseholdMedicalTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.devkingdom.householdmedicaltrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdMedicalTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdMedicalTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkingdom/householdmedicaltrackingsystem/HouseholdMedicalTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkingdom.householdmedicaltrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdMedicalTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

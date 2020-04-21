e385b258c1d4528d6fc3d13405a1a6e5350ee06f
paulin <spextreme@gmail.com>
2020-04-20 22:13:12 -0400

    Added the start of the entity definitions for the database model.

---
+++ src/main/java/com/devkingdom/householdmedicaltrackingsystem/entities/BusinessEntity.java
@@ -0,0 +1,107 @@
+/**
+ *
+ */
+package com.devkingdom.householdmedicaltrackingsystem.entities;
+
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Represents a business.
+ */
+@Entity(name = "business")
+public class BusinessEntity {
+    /**
+     * The set of addresses for this business.
+     */
+    // TODO add one to many.
+    private List<AddressEntity> addresses;
+    /**
+     * The alternate name for this business.
+     */
+    @Column(name = "alt_name")
+    private String altName;
+    /**
+     * The database id.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "business_id", nullable = false)
+    private int id;
+    /**
+     * The business primary name.
+     */
+    @Basic
+    private String name;
+
+    /**
+     * Gets the addresses
+     * @return The addresses
+     */
+    public List<AddressEntity> getAddresses() {
+        return addresses;
+    }
+
+    /**
+     * Gets the altName
+     * @return The altName
+     */
+    public String getAltName() {
+        return altName;
+    }
+
+    /**
+     * Gets the id
+     * @return The id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Gets the name
+     * @return The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the addresses.
+     * @param addresses The addresses to set
+     */
+    public void setAddresses(List<AddressEntity> addresses) {
+        this.addresses = addresses;
+    }
+
+    /**
+     * Sets the altName.
+     * @param altName The altName to set
+     */
+    public void setAltName(String altName) {
+        this.altName = altName;
+    }
+
+    /**
+     * Sets the id.
+     * @param id The id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Sets the name.
+     * @param name The name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/devkingdom/householdmedicaltrackingsystem/models/GenderEnum.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package com.devkingdom.householdmedicaltrackingsystem.models;
+
+/**
+ * Represents gender.
+ */
+public enum GenderEnum {
+    /**
+     * The female gender.
+     */
+    Female,
+    /**
+     * The male gender.
+     */
+    Male
+}
